3fea281635eed9562e4f5130d1e5a65bd5acde9e
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-19 00:05:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ class TicTacToe
   end
   #won?
   def won?
-    
+
     WIN_COMBINATIONS.each{|win_combination|
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     # grab each index from the win_combination that composes a win.
