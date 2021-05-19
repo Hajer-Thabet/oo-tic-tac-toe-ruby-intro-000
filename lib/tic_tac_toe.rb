7727b59ef0548274b92e94efc9cfa5f4ffef4230
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-19 00:12:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,6 +113,23 @@ class TicTacToe
    end
   end
 
+  
+    #winner
+  def winner
+    index = []
+    index = won?
+    if index == false
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
+  end
+    
+
 
 
 
